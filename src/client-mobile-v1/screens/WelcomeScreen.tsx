34b214bfc487987085e0a325db2ec98373789d86
Damjan Kovacic <letecnez@gmail.com>
2024-01-09 22:55:26 +0100

    Limited button width on WelcomeScreen

--- src/client-mobile-v1/screens/WelcomeScreen.tsx
+++ src/client-mobile-v1/screens/WelcomeScreen.tsx
@@ -50,6 +50,7 @@ const styles = StyleSheet.create({
     },
     buttonsContainer: {
         width: '100%',
+        maxWidth: 420,
         alignItems: 'stretch',
         marginBottom: 32,
     },
