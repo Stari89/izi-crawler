55d05bfa37760c6ba33b83d417eaa9fb6cf7be59
Damjan Kovacic <letecnez@gmail.com>
2024-01-09 20:52:33 +0100

    Installed dependencies for react-native-paper to work on the web platform

--- src/client-mobile-v1/App.tsx
+++ src/client-mobile-v1/App.tsx
@@ -1,12 +1,17 @@
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
-import { PaperProvider } from 'react-native-paper';
+import { StyleSheet, View } from 'react-native';
+import { PaperProvider, useTheme } from 'react-native-paper';
+import { Text } from 'react-native-paper';
 
 export default function App() {
+    const theme = useTheme();
+
     return (
         <PaperProvider>
-            <View style={styles.container}>
-                <Text>Open up App.tsx to start working on your app!</Text>
+            <View style={[styles.container, { backgroundColor: theme.colors.background }]}>
+                <Text variant="bodyLarge" theme={{ colors: { onSurface: theme.colors.onBackground } }}>
+                    Izi Crawler
+                </Text>
                 <StatusBar style="auto" />
             </View>
         </PaperProvider>
@@ -16,7 +21,6 @@ export default function App() {
 const styles = StyleSheet.create({
     container: {
         flex: 1,
-        backgroundColor: '#fff',
         alignItems: 'center',
         justifyContent: 'center',
     },
