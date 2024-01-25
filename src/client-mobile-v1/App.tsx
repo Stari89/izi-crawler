675b6908b19bda2c742acb154c7bb46f23093667
Damjan Kovacic <letecnez@gmail.com>
2024-01-25 22:40:41 +0100

    Providers are now in App and content is in AppContent for better theme utilization

--- src/client-mobile-v1/App.tsx
+++ src/client-mobile-v1/App.tsx
@@ -3,22 +3,39 @@ import { PaperProvider, useTheme } from 'react-native-paper';
 import { NavigationContainer } from '@react-navigation/native';
 import { createNativeStackNavigator } from '@react-navigation/native-stack';
 import { LoginScreen, SignupScreen, WelcomeScreen } from './screens';
+import { Platform } from 'react-native';
+import Constants from 'expo-constants';
 
 const Stack = createNativeStackNavigator();
 
-export default function App() {
-    const theme = useTheme();
-
+const App = () => {
     return (
         <PaperProvider>
+            <AppContent />
+        </PaperProvider>
+    );
+};
+export default App;
+
+const AppContent = () => {
+    const theme = useTheme();
+    return (
+        <>
             <StatusBar style="light" backgroundColor={theme.colors.primary} />
             <NavigationContainer>
-                <Stack.Navigator>
-                    <Stack.Screen name="welcome" component={WelcomeScreen} options={{ headerShown: false }} />
+                <Stack.Navigator
+                    screenOptions={{
+                        headerStyle: { backgroundColor: theme.colors.background },
+                        headerTintColor: theme.colors.onBackground,
+                        headerShown: false,
+                        contentStyle: { paddingTop: Platform.OS === 'ios' ? 0 : Constants.statusBarHeight },
+                    }}
+                >
+                    <Stack.Screen name="welcome" component={WelcomeScreen} />
                     <Stack.Screen name="login" component={LoginScreen} />
                     <Stack.Screen name="signup" component={SignupScreen} />
                 </Stack.Navigator>
             </NavigationContainer>
-        </PaperProvider>
+        </>
     );
-}
+};
