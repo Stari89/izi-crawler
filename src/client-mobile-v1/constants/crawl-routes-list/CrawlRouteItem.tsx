790cfde6267c2b7be3519ab9cf426828dba35f88
Damjan Kovacic <letecnez@gmail.com>
2024-01-27 21:35:28 +0100

    CrawlRouteItem styling

--- src/client-mobile-v1/constants/crawl-routes-list/CrawlRouteItem.tsx
+++ src/client-mobile-v1/constants/crawl-routes-list/CrawlRouteItem.tsx
@@ -1,19 +1,59 @@
-import { View } from 'react-native';
+import { StyleSheet, View } from 'react-native';
 import { CrawlRoute } from '../../models';
-import { Text } from 'react-native-paper';
+import { Card, IconButton, Text, useTheme } from 'react-native-paper';
 
 interface CrawlRouteItemProps {
     crawlRoute: CrawlRoute;
 }
 
 const CrawlRouteItem = (props: CrawlRouteItemProps) => {
+    const theme = useTheme();
     const { crawlRoute } = props;
 
+    const handleOptionsPress = () => {};
+
     return (
-        <View>
-            <Text>{crawlRoute.name}</Text>
+        <View style={[styles.rootContainer]}>
+            <Card style={styles.card}>
+                <Card.Content>
+                    <View style={styles.cardContentView}>
+                        <IconButton
+                            style={styles.iconButton}
+                            icon={crawlRoute.favorite ? 'star' : 'star-outline'}
+                            iconColor={theme.colors.secondary}
+                        />
+                        <View style={styles.info}>
+                            <Text>{crawlRoute.name}</Text>
+                            <Text variant="bodySmall">Created: 20.11.2024</Text>
+                        </View>
+                        <IconButton style={styles.iconButton} icon="dots-vertical" onPress={handleOptionsPress} />
+                    </View>
+                </Card.Content>
+            </Card>
         </View>
     );
 };
 
 export default CrawlRouteItem;
+
+const styles = StyleSheet.create({
+    rootContainer: {
+        flex: 1,
+    },
+    card: {
+        margin: 4,
+    },
+    cardContentView: {
+        flexDirection: 'row',
+        alignItems: 'center',
+    },
+    starIcon: {
+        marginRight: 8,
+    },
+    info: {
+        flex: 1,
+    },
+    iconButton: {
+        margin: 0,
+    },
+});
--- src/client-mobile-v1/screens/CrawlRoutesScreen.tsx
+++ src/client-mobile-v1/screens/CrawlRoutesScreen.tsx
@@ -1,7 +1,7 @@
 import { useRoute } from '@react-navigation/native';
 import { useEffect, useState } from 'react';
 import { View, StyleSheet } from 'react-native';
-import { Text, useTheme } from 'react-native-paper';
+import { useTheme } from 'react-native-paper';
 import { CrawlRoute } from '../models';
 import { CRAWL_ROUTES, LOGGED_USER } from '../data/dummy-data';
 import CrawlRoutesList from '../constants/crawl-routes-list/CrawlRoutesList';
@@ -32,6 +32,5 @@ export default CrawlRoutesScreen;
 const styles = StyleSheet.create({
     rootContainer: {
         flex: 1,
-        paddingHorizontal: 16,
     },
 });
