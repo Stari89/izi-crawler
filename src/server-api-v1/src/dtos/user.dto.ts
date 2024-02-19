2c32526e5f3e869ef202f37beb3ab648baa7b289
Damjan Kovacic <letecnez@gmail.com>
2024-02-20 00:13:55 +0100

    UserDTO

---
+++ src/server-api-v1/src/dtos/user.dto.ts
@@ -0,0 +1,25 @@
+import { ApiProperty } from '@nestjs/swagger';
+import { IsNotEmpty, IsUUID, isNotEmpty } from 'class-validator';
+
+export class UserDto {
+    @ApiProperty({ type: 'string', format: 'uuid' })
+    @IsUUID()
+    @IsNotEmpty()
+    uuid: string;
+
+    @ApiProperty({ type: 'string' })
+    @IsNotEmpty()
+    fullName: string;
+
+    @ApiProperty({ type: 'email' })
+    @IsNotEmpty()
+    email: string;
+
+    @ApiProperty({ type: 'boolean' })
+    @IsNotEmpty()
+    isActive: boolean;
+
+    @ApiProperty({ type: 'boolean' })
+    @IsNotEmpty()
+    emailConfirmed: boolean;
+}
