25267a8cb28191bebe0abf0fceca8c26905cbf20
Damjan Kovacic <letecnez@gmail.com>
2024-02-19 22:56:32 +0100

    Installed typeorm and connected to database

--- src/server-api-v1/src/app.module.ts
+++ src/server-api-v1/src/app.module.ts
@@ -1,10 +1,22 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { TypeOrmModule } from '@nestjs/typeorm';
 
 @Module({
-  imports: [],
-  controllers: [AppController],
-  providers: [AppService],
+    imports: [
+        TypeOrmModule.forRoot({
+            type: 'mysql',
+            host: 'localhost',
+            port: 3306,
+            username: 'admin',
+            password: 'password',
+            database: 'db',
+            entities: [],
+            synchronize: true,
+        }),
+    ],
+    controllers: [AppController],
+    providers: [AppService],
 })
 export class AppModule {}
