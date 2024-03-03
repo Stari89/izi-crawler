00a0538471d68db9c0da28a497a6929e9a17f0b5
Damjan Kovacic <letecnez@gmail.com>
2024-03-03 17:38:15 +0100

    Generated api-client for the first time

---
+++ src/client-mobile-v1/api-client/models/index.ts
@@ -0,0 +1,11 @@
+/* tslint:disable */
+/* eslint-disable */
+export * from './AuthConfirmResponseDto';
+export * from './AuthEmailDto';
+export * from './AuthResetPasswordDto';
+export * from './AuthSignInDto';
+export * from './AuthSignUpDto';
+export * from './AuthSignUpResponseDto';
+export * from './AuthTokenDto';
+export * from './AuthUpdatePasswordDto';
+export * from './UserDto';
