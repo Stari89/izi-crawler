3f49ce9cec34bcc771056856cbcd7b1634892356
Damjan Kovačič <letecnez@gmail.com>
2024-03-10 23:45:22 +0100

    Handling all errors on auth controller

--- src/server-api-v1/src/controllers/auth.controller.ts
+++ src/server-api-v1/src/controllers/auth.controller.ts
@@ -6,11 +6,14 @@ import {
     UseGuards,
     Request,
     InternalServerErrorException,
+    ConflictException,
+    NotFoundException,
 } from '@nestjs/common';
 import {
     ApiBadRequestResponse,
     ApiConflictResponse,
     ApiInternalServerErrorResponse,
+    ApiNotFoundResponse,
     ApiOkResponse,
     ApiTags,
     ApiUnauthorizedResponse,
@@ -30,16 +33,15 @@ export class AuthController {
     @Post('sign-in')
     @ApiOkResponse({ type: AuthTokenDto, description: 'User logged in.' })
     @ApiBadRequestResponse({ type: BadRequestDto, description: 'Invalid form data.' })
-    @ApiUnauthorizedResponse({ description: 'Wrong email or password.' })
+    @ApiUnauthorizedResponse({ description: 'Incorrect email or password.' })
     @ApiInternalServerErrorResponse({ description: 'Something went wrong.' })
     async signIn(@Body() body: AuthSignInDto): Promise<AuthTokenDto> {
         const token = await this.authService.signIn(body.email, body.password).catch((err) => {
-            // console.log(err);
             switch (err.constructor) {
                 case EntityNotFoundError:
-                    throw new UnauthorizedException('Wrong email or password.');
+                    throw new UnauthorizedException('Incorrect email or password.');
                 case UnauthorizedException:
-                    throw new UnauthorizedException('Wrong email or password.');
+                    throw new UnauthorizedException('Incorrect email or password.');
                 default:
                     throw new InternalServerErrorException('Something went wrong.');
             }
@@ -51,9 +53,17 @@ export class AuthController {
     @ApiOkResponse({ description: 'User created. Request confirmation code next.' })
     @ApiBadRequestResponse({ type: BadRequestDto, description: 'Invalid form data.' })
     @ApiConflictResponse({ description: 'User already exists.' })
+    @ApiInternalServerErrorResponse({ description: 'Something went wrong.' })
     async signUp(@Body() body: AuthEmailDto): Promise<void> {
         const { email } = body;
-        await this.authService.signUp(email);
+        await this.authService.signUp(email).catch((err) => {
+            switch (err.constructor) {
+                case ConflictException:
+                    throw new ConflictException('User already exists.');
+                default:
+                    throw new InternalServerErrorException('Something went wrong.');
+            }
+        });
     }
 
     @Post('confirmation-code')
@@ -63,10 +73,16 @@ export class AuthController {
             'Generated confirmation code and sent it to email. Use accessToken in response and the code to confirm account.',
     })
     @ApiBadRequestResponse({ type: BadRequestDto, description: 'Invalid form data.' })
-    @ApiUnauthorizedResponse({ description: 'Wrong email.' })
+    @ApiNotFoundResponse({ description: 'No user with email.' })
+    @ApiInternalServerErrorResponse({ description: 'Something went wrong.' })
     async confirmationCode(@Body() body: AuthEmailDto): Promise<AuthTokenDto> {
-        const token = await this.authService.sendConfirmCode(body.email).catch(() => {
-            throw new UnauthorizedException();
+        const token = await this.authService.sendConfirmCode(body.email).catch((err) => {
+            switch (err.constructor) {
+                case EntityNotFoundError:
+                    throw new NotFoundException(`No user with email ${body.email}`);
+                default:
+                    throw new InternalServerErrorException('Something went wrong.');
+            }
         });
         return { accessToken: token };
     }
@@ -75,10 +91,16 @@ export class AuthController {
     @ApiOkResponse({ description: 'Account confrimed.' })
     @ApiBadRequestResponse({ type: BadRequestDto, description: 'Invalid form data.' })
     @ApiUnauthorizedResponse({ description: 'Invalid token or code.' })
+    @ApiInternalServerErrorResponse({ description: 'Something went wrong.' })
     @UseGuards(AuthGuard)
     async confirmAccount(@Request() req: { user: { username: string } }, @Body() body: AuthConfirmDto): Promise<void> {
-        await this.authService.confirmAccount(req.user.username, body.confirmationCode).catch(() => {
-            throw new UnauthorizedException();
+        await this.authService.confirmAccount(req.user.username, body.confirmationCode).catch((err) => {
+            switch (err.constructor) {
+                case EntityNotFoundError:
+                    throw new UnauthorizedException('Incorrect email or confirmation code.');
+                default:
+                    throw new InternalServerErrorException('Something went wrong.');
+            }
         });
     }
 
@@ -86,14 +108,20 @@ export class AuthController {
     @ApiOkResponse({ description: 'Password was reset.' })
     @ApiBadRequestResponse({ type: BadRequestDto, description: 'Invalid form data.' })
     @ApiUnauthorizedResponse({ description: 'Invalid token.' })
+    @ApiInternalServerErrorResponse({ description: 'Something went wrong.' })
     @UseGuards(AuthGuard)
     async resetPassword(
         @Request() req: { user: { username: string } },
         @Body() body: AuthSafePasswordDto,
     ): Promise<void> {
         const { password } = body;
-        await this.authService.resetPassword(req.user.username, password).catch(() => {
-            throw new UnauthorizedException();
+        await this.authService.resetPassword(req.user.username, password).catch((err) => {
+            switch (err.constructor) {
+                case EntityNotFoundError:
+                    throw new UnauthorizedException('Incorrect email or confirmation code.');
+                default:
+                    throw new InternalServerErrorException('Something went wrong.');
+            }
         });
     }
 
@@ -107,8 +135,15 @@ export class AuthController {
         @Body() body: AuthUpdatePasswordDto,
     ): Promise<void> {
         const { password, oldPassword } = body;
-        await this.authService.updatePassword(req.user.username, oldPassword, password).catch(() => {
-            throw new UnauthorizedException();
+        await this.authService.updatePassword(req.user.username, oldPassword, password).catch((err) => {
+            switch (err.constructor) {
+                case EntityNotFoundError:
+                    throw new UnauthorizedException('Incorrect email or password.');
+                case UnauthorizedException:
+                    throw new UnauthorizedException('Incorrect email or password.');
+                default:
+                    throw new InternalServerErrorException('Something went wrong.');
+            }
         });
     }
 }
